92d54cb2f018112d9e8bf43e39ff779bd84ffad1
Abel Galarza <47190761+AbelGM98@users.noreply.github.com>
2021-12-06 12:04:37 -0600

    Add files via upload

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,16 @@
+# CMake generated Testfile for 
+# Source directory: /home/agm/Mobile-Robots-2022-1/catkin_ws/src
+# Build directory: /home/agm/Mobile-Robots-2022-1/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("pocketsphinx")
+subdirs("config_files")
+subdirs("hardware/gazebo_envs")
+subdirs("hardware/justina_description")
+subdirs("surge_et_ambula")
+subdirs("custom_msgs")
+subdirs("justina_gui")
+subdirs("hardware/occupancy_grid_utils")
+subdirs("students")
---
+++ catkin_ws/build/surge_et_ambula/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/agm/Mobile-Robots-2022-1/catkin_ws/src/surge_et_ambula
+# Build directory: /home/agm/Mobile-Robots-2022-1/catkin_ws/build/surge_et_ambula
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
